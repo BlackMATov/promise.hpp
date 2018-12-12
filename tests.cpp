59778fd048ab0cfab048d30ab7d30fef1b44bae4
BlackMATov <blackmatov@gmail.com>
2018-12-12 22:36:44 +0700

    bonuses: scheduler

--- tests.cpp
+++ tests.cpp
@@ -12,9 +12,11 @@
 
 #include "jobber.hpp"
 #include "promise.hpp"
+#include "scheduler.hpp"
 
 namespace jb = jobber_hpp;
 namespace pr = promise_hpp;
+namespace sd = scheduler_hpp;
 
 namespace
 {
@@ -1227,7 +1229,7 @@ TEST_CASE("jobber") {
                 for ( std::size_t i = 0; i < gp.size(); ++i ) {
                     gp[i] = g.async([](float angle){
                         return std::sin(angle);
-                    }, i);
+                    }, static_cast<float>(i));
                 }
                 return std::accumulate(gp.begin(), gp.end(), 0.f,
                     [](float r, pr::promise<float>& f){
@@ -1244,5 +1246,104 @@ TEST_CASE("jobber") {
             r1 += std::sin(static_cast<float>(i));
         }
         REQUIRE(r0 == Approx(r1 * 50.f).margin(0.01f));
+    }
+}
+
+TEST_CASE("scheduler") {
+    {
+        sd::scheduler s;
+        auto pv0 = s.schedule([](){
+            throw std::exception();
+        });
+        s.process_all_tasks();
+        REQUIRE_THROWS_AS(pv0.get(), std::exception);
+    }
+    {
+        auto pv0 = pr::promise<int>();
+        {
+            sd::scheduler s;
+            pv0 = s.schedule([](){
+                return 42;
+            });
+        }
+        REQUIRE_THROWS_AS(pv0.get(), sd::scheduler_cancelled_exception);
+    }
+    {
+        sd::scheduler s;
+        int counter = 0;
+        s.schedule([&counter](){ ++counter; });
+        REQUIRE(counter == 0);
+        s.process_all_tasks();
+        REQUIRE(counter == 1);
+        s.schedule([&counter](){ ++counter; });
+        s.schedule([&counter](){ ++counter; });
+        REQUIRE(counter == 1);
+        s.process_all_tasks();
+        REQUIRE(counter == 3);
+    }
+    {
+        sd::scheduler s;
+        int counter = 0;
+        for ( std::size_t i = 0; i < 50; ++i ) {
+            s.schedule([&counter](){
+                ++counter;
+                std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            });
+        }
+        s.process_tasks_for(std::chrono::milliseconds(-1));
+        s.process_tasks_for(std::chrono::milliseconds(0));
+        REQUIRE(counter == 0);
+        s.process_tasks_for(std::chrono::milliseconds(100));
+        REQUIRE(counter > 2);
+        REQUIRE(counter < 50);
+        s.process_tasks_for(std::chrono::seconds(3));
+        REQUIRE(counter == 50);
+    }
+    {
+        sd::scheduler s;
+        int counter = 0;
+        for ( std::size_t i = 0; i < 50; ++i ) {
+            s.schedule([&counter](){
+                ++counter;
+                std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            });
+        }
+
+        const auto time_now = [](){
+            return std::chrono::high_resolution_clock::now();
+        };
+
+        const auto b = time_now();
+
+        s.process_tasks_until(time_now() - std::chrono::milliseconds(1));
+        s.process_tasks_until(time_now());
+        REQUIRE(counter == 0);
+        s.process_tasks_until(time_now() + std::chrono::milliseconds(100));
+        REQUIRE(time_now() - b > std::chrono::milliseconds(50));
+        REQUIRE(counter > 2);
+        REQUIRE(counter < 50);
+        s.process_tasks_until(time_now() + std::chrono::seconds(3));
+        REQUIRE(counter == 50);
+    }
+    {
+        sd::scheduler s;
+        std::string accumulator;
+        s.schedule(sd::scheduler_priority::lowest, [](std::string& acc){
+            acc.append("o");
+        }, std::ref(accumulator));
+        s.schedule(sd::scheduler_priority::below_normal, [](std::string& acc){
+            acc.append("l");
+        }, std::ref(accumulator));
+        s.schedule(sd::scheduler_priority::highest, [](std::string& acc){
+            acc.append("h");
+        }, std::ref(accumulator));
+        s.schedule(sd::scheduler_priority::above_normal, [](std::string& acc){
+            acc.append("e");
+        }, std::ref(accumulator));
+        s.schedule(sd::scheduler_priority::normal, [](std::string& acc){
+            acc.append("l");
+        }, std::ref(accumulator));
+        s.process_all_tasks();
+        REQUIRE(accumulator == "hello");
     }
 }
