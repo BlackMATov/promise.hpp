03669c949f3048b6849b1a163072e5a08b2b8fba
BlackMATov <blackmat@mail.ru>
2021-01-12 03:15:11 +0700

    new ci scripts

--- scripts/build_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-BUILD_DIR=`dirname "$BASH_SOURCE"`/../build
-mkdir -p $BUILD_DIR/Debug
-cd $BUILD_DIR/Debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
-cmake --build .
-ctest --verbose
-cd ../..
